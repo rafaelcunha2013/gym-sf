5a8f0f67ab62e78408efd3e55fbf7ad6a6b854ef
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-09-08 13:20:37 +0200

    Add import of the package FourRoom

--- gym_sf/four_room/__init__.py
+++ gym_sf/four_room/__init__.py
@@ -1,7 +1,1 @@
-from gym.envs.registration import register
-
-register(
-    id='four-room-v0',
-    entry_point='gym_sf.four_room:FourRoom',
-    max_episode_steps=5000,
-)
+from gym_sf.four_room.four_room import FourRoom
