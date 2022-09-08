c3b0dc8f399138116b82e8aff0860037b2c40149
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-09-08 14:04:15 +0200

    receive terminated and truncated

--- gym_sf/four_room/example.py
+++ gym_sf/four_room/example.py
@@ -1,24 +1,26 @@
 import numpy as np
+import gym
 from four_room import FourRoom
 # from render import Render
 
-env = FourRoom(render_mode='rgb_array')
-done = False
+# env = gym.make("gym_sf/four-room-v0")
+env = gym.make("gym_sf:gym_sf/four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
+# env = FourRoom(render_mode='rgb_array')
+terminated = False
+truncated = False
 env.reset()
-a = []
 
-for _ in range(50):
-    a.append(env.render())
+for _ in range(500):
+    # a.append(env.render())
+    env.render()
     action = env.action_space.sample()
     if np.random.random() < 0.20:
         if np.random.random() < 0.50:
             action = 2
         else:
             action = 1
-    next_state, reward, done = env.step(action)
+    next_state, reward, terminated, truncated, _ = env.step(action)
 
-    if done:
+    if terminated or truncated:
         env.reset()
-        print('hi')
 env.close()
-print(a)
