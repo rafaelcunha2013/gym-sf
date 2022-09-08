84c0640f3fad60833b77dde48ea20506f8b20a1d
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-09-08 15:34:47 +0200

    deleted extra lines with old commented code

--- gym_sf/__init__.py
+++ gym_sf/__init__.py
@@ -5,6 +5,3 @@ register(
     entry_point='gym_sf.four_room:FourRoom',
     max_episode_steps=5000,
 )
-# import gym_sf.four_room
-# from gym_sf.four_room.four_room import FourRoom
-# from gym_sf.four_room.render import Render
