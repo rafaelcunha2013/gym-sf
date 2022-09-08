c850d64e3e714fd8a4f6ca74677c691884bd487a
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-09-08 16:22:53 +0200

    Add license

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='gym-sf',
       author="Rafael F Cunha",
       author_email="rafaelcunha2013@gmail.com",
       url="https://github.com/rafaelcunha2013/gym-sf",
+      license='MIT',
       python_requires=">=3.8",
       install_requires=['gym==0.25.2',
                         'numpy',
@@ -13,3 +14,5 @@ setup(name='gym-sf',
       )
 
 # https://www.gymlibrary.dev/content/environment_creation/
+# https://towardsdatascience.com/create-your-own-python-package-and-publish-it-into-pypi-9306a29bc116
+
