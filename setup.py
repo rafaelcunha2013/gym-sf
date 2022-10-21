1ebb63e760aa627196a7f5f97f032e934fe760e1
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-10-21 17:16:44 +0200

    add package to setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='gym-sf',
-      version='0.0.1',
+      version='0.0.2',
       description='Environments adapted to the use of successor features',
       author="Rafael F Cunha",
       author_email="rafaelcunha2013@gmail.com",
       url="https://github.com/rafaelcunha2013/gym-sf",
       license='MIT',
+      packages=['gym_sf'],
       python_requires=">=3.8",
       install_requires=['gym==0.25.2',
                         'numpy',
