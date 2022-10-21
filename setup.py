25f6100d9d698c0bba45c5976ced6fe4d52eb618
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-10-21 17:40:20 +0200

    fix package=packages

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 packages = [package for package in find_packages() if package.startswith("gym_sf")]
 
 setup(name='gym-sf',
-      version='0.0.3',
+      version='0.0.4',
       description='Environments adapted to the use of successor features',
       author="Rafael F Cunha",
       author_email="rafaelcunha2013@gmail.com",
       url="https://github.com/rafaelcunha2013/gym-sf",
       license='MIT',
-      packages=['gym_sf'],
+      packages=packages,
       python_requires=">=3.8",
       install_requires=['gym==0.25.2',
                         'numpy',
