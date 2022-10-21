8bceb90dc23115da0673b79c73cd8d93ca91426d
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-10-21 16:01:12 +0200

    importing and calling the environment correctly.

--- gym_sf/four_room/example.py
+++ gym_sf/four_room/example.py
@@ -1,12 +1,14 @@
 import numpy as np
 import gym
+# import four_room
 import gym_sf
 
 # env = gym.make("gym_sf/four-room-v0")
 # env = gym_sf.make("gym_sf:gym_sf/four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
-env = gym.make("gym_sf/four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
+# env = gym.make("gym_sf/four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
 # env = gym.make("four_room:sf-four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
 # env = FourRoom(render_mode='rgb_array')
+env = gym.make("four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
 terminated = False
 truncated = False
 env.reset()
