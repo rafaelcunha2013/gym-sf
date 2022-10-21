51e11e83c95ea28e1b95323e63df96133295b1e5
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-10-21 15:30:32 +0200

    Add register in gym env

--- gym_sf/four_room/__init__.py
+++ gym_sf/four_room/__init__.py
@@ -1,4 +1,9 @@
-from gym_sf.four_room.four_room import FourRoom
-from gym_sf.four_room.render import Render
+from .render import Render
+from gym.envs.registration import register
 
+register(
+    id='four-room-v0',
+    entry_point='gym_sf.four_room.four_room:FourRoom',
+    max_episode_steps=5000,
+)
 
