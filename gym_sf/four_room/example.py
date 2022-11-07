4c6df2136347487caa9bd6b603ce6d511c0b1c72
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-11-07 19:31:00 +0100

    list of render_modes

--- gym_sf/four_room/example.py
+++ gym_sf/four_room/example.py
@@ -2,7 +2,8 @@ import numpy as np
 import gym
 import gym_sf
 
-env = gym.make("four-room-v0", render_mode='rgb_array', new_step_api=True, max_episode_steps=5000, video=True)
+render_mode = "rgb_array_list" # "rgb_array" "rgb_array_list"
+env = gym.make("four-room-v0", render_mode=render_mode, max_episode_steps=5000, video=True)
 # env = gym.make("four-room-v0", render_mode='human', new_step_api=True, max_episode_steps=5000)
 terminated = False
 truncated = False
