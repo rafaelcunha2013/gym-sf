ab068a91572d4d76da09dccbe61674a9f3203d4b
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-12-21 13:28:48 +0100

    choose video_path to record video and format of video mp4

--- gym_sf/four_room/utilities.py
+++ gym_sf/four_room/utilities.py
@@ -3,13 +3,25 @@ from datetime import datetime
 import os
 
 
-def frame_to_video(renders, path="root"):
+def frame_to_video(renders, path="root", video_format='mp4'):
     height, width, layers = renders[0].shape
     unique_id = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
-    video_name = unique_id + ".avi"
+
+    if video_format == 'avi':
+        video_name = unique_id + ".avi"
+        fourcc = 0
+
+    if video_format == 'mp4':
+        video_name = unique_id + ".mp4"
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
     if path != "root":
+        if not os.path.exists(path):
+            os.makedirs(path)
         video_name = os.path.join(path, video_name)
-    video = cv2.VideoWriter(video_name, 0, fps=20, frameSize=(width, height))
+
+    video = cv2.VideoWriter(video_name, fourcc=fourcc, fps=10, frameSize=(width, height))
+
     for frame in renders:
         video.write(frame)
     cv2.destroyAllWindows()
