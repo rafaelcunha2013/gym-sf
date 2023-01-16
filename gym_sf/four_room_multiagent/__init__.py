ecc3c9e0d8e93e5bc9582571a5cbf9bcc6f2ac8f
Rafael Cunha <rafaelcunha2013@gmail.com>
2023-01-16 18:35:56 +0100

    registering multienvironment

--- gym_sf/four_room_multiagent/__init__.py
+++ gym_sf/four_room_multiagent/__init__.py
@@ -1,15 +1,8 @@
-from .render import Render
+from gym_sf.four_room.render import Render
 from gym.envs.registration import register
-
-register(
-    id='four-room-v0',
-    entry_point='gym_sf.four_room.four_room:FourRoom',
-    max_episode_steps=5000,
-)
 
 register(
     id='four-room-multiagent-v0',
     entry_point='gym_sf.four_room_multiagent.four_room_multiagent:FourRoomMultiagent',
     max_episode_steps=5000,
 )
-
